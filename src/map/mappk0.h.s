b90e162be396a4cefa4ac0e2c4c8008c86159e5a
salvakantero <salvacantero@outlook.com>
2023-05-21 13:54:18 +0200

    -

--- src/map/mappk0.h.s
+++ src/map/mappk0.h.s
@@ -2,8 +2,8 @@
 ;; Compresor used:   zx7b
 ;; Files compressed: [ 'src/map0.bin' ]
 ;; Uncompressed:     1440 bytes
-;; Compressed:       201 bytes
-;; Space saved:      1239 bytes
+;; Compressed:       178 bytes
+;; Space saved:      1262 bytes
 ;;
 
 ;; Declaration of the compressed array and
@@ -12,7 +12,7 @@
 .globl _mappk0_end
 
 ;; Compressed and uncompressed sizes
-_mappk0_size_z == 201
+_mappk0_size_z == 178
 _mappk0_size   == 1440
 
 
