046fc49ba73efb6d5f7b09f223fab69de48bf4a1
salvakantero <salvacantero@outlook.com>
2023-06-29 20:55:56 +0200

    -

--- src/map/mappk0.h
+++ src/map/mappk0.h
@@ -2,22 +2,22 @@
 // Compresor used:   zx7b
 // Files compressed: [ 'src/map0.bin' ]
 // Uncompressed:     1440 bytes
-// Compressed:       197 bytes
-// Space saved:      1243 bytes
+// Compressed:       166 bytes
+// Space saved:      1274 bytes
 //
 
-#ifndef mappk0_197_H
-#define mappk0_197_H
+#ifndef mappk0_166_H
+#define mappk0_166_H
 
 
 // Declaration of the compressed array
-extern const unsigned char mappk0[197];
+extern const unsigned char mappk0[166];
 
 // Address of the latest byte of the compressed array (for unpacking purposes)
-#define mappk0_end      (mappk0 + 197 - 1)
+#define mappk0_end      (mappk0 + 166 - 1)
 
 // Compressed and uncompressed sizes
-#define mappk0_size_z   197
+#define mappk0_size_z   166
 #define mappk0_size     1440
 
 
--- src/map/mappk0.h.s
+++ src/map/mappk0.h.s
@@ -2,8 +2,8 @@
 ;; Compresor used:   zx7b
 ;; Files compressed: [ 'src/map0.bin' ]
 ;; Uncompressed:     1440 bytes
-;; Compressed:       197 bytes
-;; Space saved:      1243 bytes
+;; Compressed:       166 bytes
+;; Space saved:      1274 bytes
 ;;
 
 ;; Declaration of the compressed array and
@@ -12,7 +12,7 @@
 .globl _mappk0_end
 
 ;; Compressed and uncompressed sizes
-_mappk0_size_z == 197
+_mappk0_size_z == 166
 _mappk0_size   == 1440
 
 
