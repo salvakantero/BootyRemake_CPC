9cb2cf13ff9795739fe070fe5f6158eeae5a85b0
salvakantero <salvacantero@outlook.com>
2023-09-12 15:35:05 +0100

    -

--- src/map/mappk17.h
+++ src/map/mappk17.h
@@ -2,22 +2,22 @@
 // Compresor used:   zx7b
 // Files compressed: [ 'src/map17.bin' ]
 // Uncompressed:     1440 bytes
-// Compressed:       221 bytes
-// Space saved:      1219 bytes
+// Compressed:       220 bytes
+// Space saved:      1220 bytes
 //
 
-#ifndef mappk17_221_H
-#define mappk17_221_H
+#ifndef mappk17_220_H
+#define mappk17_220_H
 
 
 // Declaration of the compressed array
-extern const unsigned char mappk17[221];
+extern const unsigned char mappk17[220];
 
 // Address of the latest byte of the compressed array (for unpacking purposes)
-#define mappk17_end      (mappk17 + 221 - 1)
+#define mappk17_end      (mappk17 + 220 - 1)
 
 // Compressed and uncompressed sizes
-#define mappk17_size_z   221
+#define mappk17_size_z   220
 #define mappk17_size     1440
 
 
--- src/map17.h
+++ src/map17.h
@@ -1,5 +1,5 @@
 //
-// File assets/map17.tmx converted to csv using cpct_tmx2data [Sun Sep 10 18:32:50 2023]
+// File assets/map17.tmx converted to csv using cpct_tmx2data [Tue Sep 12 16:05:42 2023]
 //   * Visible Layers:  1
 //   * Layer Width:     40
 //   * Layer Height:    36
